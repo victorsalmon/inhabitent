cc78d2336e1553dfee8148487b372b5c9bfbacfc
Victor Salmon <vasalmon@hotmail.com>
2017-05-24 09:17:48 -0700

    Added custom about page back-end for user

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -8,13 +8,14 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="container-footer">
 					<?php dynamic_sidebar( 'sidebar-2' ); ?>
-				<!--<div class="contact-info">
-					<p><i class="fa fa-envelope"></i>info@inhabitent.com-->
-				<div class="site-info">
+				</div>
+				<div class="copyright">
 					<span>Copyright &#169; 2016 Inhabitent</span>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
+			
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -1,1 +1,41 @@
 <!--The about page will go here and will be a subset of pages lacking a sidebar-->
+
+<?php
+/**
+ * The template for displaying all pages.
+ * Template Name: About Page
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<?php
+    if ( has_post_thumbnail() ) :	
+      $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+		endif;
+    ?>
+	<header class="entry-header" style="background-image: url('<?php echo $thumb['0'];?>')">
+  
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
