dea6959f41f4b0252179152fcdb3c048a9c769a7
Victor Salmon <vasalmon@hotmail.com>
2017-05-25 15:48:36 -0700

    Styled footer and added footer logo

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -65,3 +65,50 @@ function inhabitent_login_title() {
 }
 add_filter( 'login_headertitle', 'inhabitent_login_title' );
 
+function inhabitent_wp_trim_excerpt( $text ) {
+/**
+ * Customize excerpt length and style.
+ *
+ * @param  string The raw post content.
+ * @return string
+ **/
+	$raw_excerpt = $text;
+	
+	if ( '' == $text ) {
+		// retrieve the post content
+		$text = get_the_content('');
+		
+		// delete all shortcode tags from the content
+		$text = strip_shortcodes( $text );
+		
+		$text = apply_filters( 'the_content', $text );
+		$text = str_replace( ']]>', ']]&gt;', $text );
+		
+		// indicate allowable tags
+		$allowed_tags = '<p>,<a>,<em>,<strong>,<blockquote>,<cite>';
+		$text = strip_tags( $text, $allowed_tags );
+		
+		// change to desired word count
+		$excerpt_word_count = 50;
+		$excerpt_length = apply_filters( 'excerpt_length', $excerpt_word_count );
+		
+		// create a custom "more" link
+		$excerpt_end = '<span>[...]</span><p><a href="' . get_permalink() . '" class="read-more">Read more &rarr;</a></p>'; // modify excerpt ending
+		$excerpt_more = apply_filters( 'excerpt_more', ' ' . $excerpt_end );
+		
+		// add the elipsis and link to the end if the word count is longer than the excerpt
+		$words = preg_split( "/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY );
+		
+		if ( count( $words ) > $excerpt_length ) {
+			array_pop( $words );
+			$text = implode( ' ', $words );
+			$text = $text . $excerpt_more;
+		} else {
+			$text = implode( ' ', $words );
+		}
+	}
+	
+	return apply_filters( 'wp_trim_excerpt', $text, $raw_excerpt );
+}
+remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
+add_filter( 'get_the_excerpt', 'inhabitent_wp_trim_excerpt' );
