76ac160e62604ec0e9793ad931b4fb7575f7d137
Victor Salmon <vasalmon@hotmail.com>
2017-05-25 21:26:54 -0700

    Refactored navigation scss and styled archive-product.php

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -7,40 +7,37 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<header class="page-header">
-				<h1 class="page-title"> Shop Stuff </h1>
-					<?php 
-						$product_types = get_terms( 'product-type' ); 
-						if ( !empty($product_types) && !is_wp_error($product_types)) : ?>
-								<?php foreach ( $product_types as $product_type ) : ?>
-									<div class="list-product-type">
-										<a href="<?php echo get_term_link($product_type);?>">
-											<h3><?php echo $product_type->name;?></h3>
-										</a>
-									</div>
-								<?php endforeach; ?>
-					<?php endif; ?>
-			</header> <!--Page Header-->
+		<header class="gallery-header">
+			<h1 class="page-title"> Shop Stuff </h1>
+			<ul class="product-types">
+				<?php 
+					$product_types = get_terms( 'product-type' ); 
+					if ( !empty($product_types) && !is_wp_error($product_types)) : ?>
+							<?php foreach ( $product_types as $product_type ) : ?>
+								<li class="list-product-type">
+									<a href="<?php echo get_term_link($product_type);?>">
+										<h3><?php echo $product_type->name;?></h3>
+									</a>
+								</li>
+							<?php endforeach; ?>
+				<?php endif; ?>
+			</ul>
+		</header> <!--Page Header-->
 
-			<?php if ( have_posts() ) : ?>
-				<?php
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-				<div class="product-gallery">
-					<?php /* Start the Loop */ ?>
+		<?php if ( have_posts() ) : ?>
+			<div class="product-gallery">
+				<?php /* Start the Loop */ ?>
 
-					<?php while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-						<div class="product-panel">
-							<?php	get_template_part( 'template-parts/content', 'product-small' ); ?>
-						</div>
+					<div class="product-panel">
+						<?php	get_template_part( 'template-parts/content', 'product-small' ); ?>
+					</div>
 
-					<?php endwhile; ?>
-				</div>
-			<?php endif; ?>
+				<?php endwhile; ?>
+			</div>
+		<?php endif; ?>
 
-		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -20,10 +20,6 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<!--<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div>-->
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<a href="<?php echo get_home_url();?>">
 						<div class="logo-tent">
