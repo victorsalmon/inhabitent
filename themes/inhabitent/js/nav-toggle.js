9a522ecbc4d3f4c3c3a2e6a6967cad050d3f3665
Victor Salmon <vasalmon@hotmail.com>
2017-06-09 16:24:14 -0700

    Styled custom nav bar first draft, position fixed

--- themes/inhabitent/js/nav-toggle.js
+++ themes/inhabitent/js/nav-toggle.js
@@ -1,6 +1,7 @@
 (function($){
   $(document).ready(function(){
 
+
     $('.open-the-search-form').on('click', function() {
       $('.main-navigation .search-field').animate({width:'toggle'}, 0);
       $('.main-navigation .search-field').focus();      
@@ -11,17 +12,37 @@
 
     });
 
-    //Scrolling
-    $('.main-navigation').addClass('custom-nav');
-    $(window).on('scroll', function(){
-      var windowHeight = $('.hero').height();
-      var posEndScroll = Math.round( $(window).scrollTop() );
-      if (posEndScroll >= windowHeight){
-        $('.main-navigation').removeClass('custom-nav');
-      } else {
-        $('.main-navigation').addClass('custom-nav');
-      }
-    });
+// (function ($) {
+//   $(window).scroll(function() {
+//     var winH = $(window).height();
+//   if ($(window).scrollTop() > winH) {
+//     $('.home header, .page-template-about header').addClass('green-navigation');
+//     $('.home header, .page-template-about header').removeClass('site-header');
 
-  })
-})(jQuery);
+    //Scrolling
+    if (
+      $('body').hasClass('page-template-page-about') ||
+      $('body').hasClass('home') ||
+      $('body').hasClass('single-adventure') ){
+        $('.main-navigation').addClass('custom-nav');
+        $('.site-content').css("padding-top", "0");
+        $(window).on('scroll', function(){
+            var a = $('.hero').height(),
+                b = $('.product-image').height(),
+                c = $('.page-template-page-about .entry-header').height(),
+                windowHeight = Math.max(a, b, c);
+            console.log (a, b, c, windowHeight);
+            
+          var posEndScroll = Math.round( $(window).scrollTop() );
+          if (posEndScroll >= windowHeight){
+            $('.main-navigation').removeClass('custom-nav');
+          } else {
+            $('.main-navigation').addClass('custom-nav');
+            $('.site-content').css("padding-top", "0");
+          }
+        })
+    }
+
+
+  }) // document.ready
+})(jQuery); //IIFE
